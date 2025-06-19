66111b6334948f0c4a1cbd5cc7f4ad964c780aeb
upn_i <upn_i@DESKTOP-1KG2GS2>
2025-06-19 04:10:17 +0200

    Agregar archivos de proyecto.

---
+++ T3_Tenorio_Maurizio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace T3_Tenorio_Maurizio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
